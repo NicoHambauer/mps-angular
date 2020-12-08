750af4d7c031926ce72dee05a3093b3965f1b9c7
NicoHambauer <nico.hambauer@fau.de>
2020-12-08 12:19:35 +0100

    moved first section to sections folder and changed layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { LoginButtonComponent } from './components/login-button/login-button.component';
-import { FirstSectionComponent } from './components/first-section/first-section.component';
+import { FirstSectionComponent } from './components/sections/first-section/first-section.component';
 import { SecondSectionComponent } from './components/sections/second-section/second-section.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { GraphComponent } from './components/graph/graph.component';
